60e5a0282cf7201b082fab49b7a3514ace5c06bd
danibene <34680344+danibene@users.noreply.github.com>
2022-08-17 22:43:25 -0400

    add hashtags to avoid subsequent tweets being duplicates

--- write_tweet.py
+++ write_tweet.py
@@ -1,5 +1,8 @@
 import os
 import tweepy
+import numpy as np
+
+from json_tricks import load, dump
 
 
 def twitter_authentication():
@@ -10,10 +13,33 @@ def twitter_authentication():
     return tweepy.API(auth)
 
 
-def tweet(api):
-    api.update_status("Still not done")
+def tweet(api, status="Still not done"):
+    api.update_status(status)
+
+
+def get_new_hashtag(d):
+    hashtag = d["last_hashtag_tweeted"]
+    while hashtag == d["last_hashtag_tweeted"]:
+        hashtag_index = np.random.randint(0, high=len(d["hashtags"]))
+        hashtag = d["hashtags"][hashtag_index]
+    return hashtag
+
+
+def update_hashtag(json_path):
+    with open(json_path, "r") as json_file:
+        d = load(json_file)
+
+    hashtag = get_new_hashtag(d)
+
+    with open(json_path, "w") as json_file:
+        dump(d, json_file)
+
+    return hashtag
 
 
 if __name__ == "__main__":
     api = twitter_authentication()
-    tweet(api)
+    json_path = "tweet_info.json"
+    hashtag = update_hashtag(json_path)
+	status = "Still not done #" + hashtag
+    tweet(api, status=status)
